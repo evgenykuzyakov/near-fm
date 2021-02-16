a211058add0169b0e90ade086e76a7805ab1b8f4
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-02-15 23:18:04 -0600

    Updates

---
+++ precache-manifest.f9063f2e62c0fe3119cd9a47d429e67c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54ba637f3aefddd9b838d3add5e5e71",
+    "url": "/near-vodka/index.html"
+  },
+  {
+    "revision": "612bd4276ff609c1d442",
+    "url": "/near-vodka/static/css/2.f747b8b9.chunk.css"
+  },
+  {
+    "revision": "7b0f790b501f33cad5dd",
+    "url": "/near-vodka/static/css/main.95bd0358.chunk.css"
+  },
+  {
+    "revision": "612bd4276ff609c1d442",
+    "url": "/near-vodka/static/js/2.4e769aea.chunk.js"
+  },
+  {
+    "revision": "a327c67d57be3bdec454207314626087",
+    "url": "/near-vodka/static/js/2.4e769aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0f790b501f33cad5dd",
+    "url": "/near-vodka/static/js/main.a19c046b.chunk.js"
+  },
+  {
+    "revision": "8fc6881eefc6e5a7158b",
+    "url": "/near-vodka/static/js/runtime-main.31ad8391.js"
+  },
+  {
+    "revision": "f18e89c9f1e9b7080559da5bc2926895",
+    "url": "/near-vodka/static/media/near_vodka_logo.f18e89c9.png"
+  }
+]);
